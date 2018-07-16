a7eb195edca832565c128bfb627ace72488da26f
Abousyllaba Ndiaye <abousyllaba.ndiaye@devoteam.lu>
2018-07-16 11:50:11 +0200

    add component produit

---
+++ src/app/produit/produit.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+import {Produit} from '../shared/produit';
+import {ProduitMockService} from './produit.mock.service';
+
+@Component({
+  selector: 'app-produit',
+  templateUrl: './produit.component.html',
+  styleUrls: ['./produit.component.css']
+})
+export class ProduitComponent implements OnInit {
+  produits: Produit[];
+  constructor(private produitServie:ProduitMockService) { }
+
+  ngOnInit() {
+    this.produits = this.produitServie.getProduits();
+  }
+
+}
