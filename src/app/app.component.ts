85395530664c1c5bdb182419c33be00c00f1a9cb
Abousyllaba Ndiaye <abousyllaba.ndiaye@devoteam.lu>
2018-07-16 17:28:00 +0200

    ajout de sidebar et navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  showHideSideBar:boolean=false;
+  onShowSideBarChange(showHideSideBar){
+    this.showHideSideBar = showHideSideBar;
+  }
 }
