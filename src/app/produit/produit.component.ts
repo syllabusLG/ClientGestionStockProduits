c5a20078771dafbbf948602e8669739c773af46d
Abousyllaba Ndiaye <abousyllaba.ndiaye@devoteam.lu>
2018-07-17 15:50:27 +0200

    crud produits

--- src/app/produit/produit.component.ts
+++ src/app/produit/produit.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {ActivatedRoute} from '@angular/router';
 
 import {Produit} from '../shared/produit';
-import {ProduitMockService} from './produit.mock.service';
+import {ProduitService} from './produit.service';
 
 @Component({
   selector: 'app-produit',
@@ -10,10 +12,60 @@ import {ProduitMockService} from './produit.mock.service';
 })
 export class ProduitComponent implements OnInit {
   produits: Produit[];
-  constructor(private produitServie:ProduitMockService) { }
 
-  ngOnInit() {
-    this.produits = this.produitServie.getProduits();
+  //variable pour le formulaire
+  produitForm: FormGroup;
+  operation:string='add';
+  selectedProduit:Produit;
+  constructor(private produitServie:ProduitService, private fb: FormBuilder, private route: ActivatedRoute) {
+    this.createFrom();
   }
 
+  ngOnInit() {
+    this.initProduit();
+    this.produits = this.route.snapshot.data.produits;
+  }
+  createFrom(){
+    this.produitForm = this.fb.group({
+      ref: ['', Validators.required],
+      quantite: '',
+      prixUnitaire: ''
+    });
+  }
+  loadProduits(){
+    this.produitServie.getProduits().subscribe(
+      data=>{this.produits = data},
+      error=>{console.log("An error was occured.")},
+      ()=>{console.log('loading produits was done')}
+    );
+  }
+  addProduit(){
+    const p = this.produitForm.value;
+    this.produitServie.addProduit(p).subscribe(
+      res =>{
+        this.initProduit();
+        this.loadProduits();
+      }
+    );
+  }
+  updateProduit(){
+    this.produitServie.updateProduit(this.selectedProduit).subscribe(
+      res=>{
+        this.initProduit();
+        this.loadProduits();
+      }
+    )
+  }
+  initProduit(){
+    this.selectedProduit = new Produit();
+    this.createFrom();
+  }
+  deleteProduit(){
+    this.produitServie.deleteProduit(this.selectedProduit.ref).subscribe(
+      res=>{
+        this.selectedProduit = new Produit();
+        this.loadProduits();
+      }
+    );
+  }
 }
