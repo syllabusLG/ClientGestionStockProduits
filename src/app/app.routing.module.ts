6e3eae89860919c3e6d9d90fa2f8a844b7770f33
Abousyllaba Ndiaye <abousyllaba.ndiaye@devoteam.lu>
2018-07-19 10:12:31 +0200

    authentication in memory

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -4,24 +4,39 @@ import {RouterModule, Routes} from '@angular/router';
 import {ProduitComponent} from './produit/produit.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {ProduitResolver} from './produit/produit.resolver';
+import {LoginComponent} from './login/login.component';
+import {HomeComponent} from './home/home.component';
 
 export const appRoutes: Routes = [
   {
-    path: 'produit',
-    component: ProduitComponent,
-    resolve: {
-      produits: ProduitResolver
-    }
+    path: 'home',
+    component: HomeComponent,
+    children: [
+      {
+        path: 'produit',
+        component: ProduitComponent,
+        resolve: {
+          produits: ProduitResolver
+        },
+        outlet: 'contentOutlet'
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+        outlet: 'contentOutlet'
+      },
+    ]
   },
-  {path: 'dashboard', component: DashboardComponent},
-  {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
+  {path: 'login', component: LoginComponent},
+
+  {path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
 
 @NgModule({
   imports:[
     RouterModule.forRoot(
       appRoutes,
-      {enableTracing: true}
+      {enableTracing: false}
     )
   ],
   exports: [RouterModule],
